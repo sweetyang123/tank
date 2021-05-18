353929fb53c10c4905e5fc14b940143b16f99c76
sweetyang123 <84315764+sweetyang123@users.noreply.github.com>
2021-05-18 10:55:37 +0800

    Create Tank.java
    封装一个坦克对象，并配一只画笔，画出自己
    
    Co-Authored-By: yangtian <2020176+yangtian@users.noreply.github.com>

---
+++ src/main/java/frame/Tank.java
@@ -0,0 +1,41 @@
+package frame;
+
+import java.awt.*;
+
+public class Tank {
+    private int x,y;
+    private int height,width;
+    private Dir dir;
+    private static  final int SPEED=50;
+
+    public Tank(int x, int y,int height,int width,Dir dir) {
+        this.x = x;
+        this.y = y;
+        this.height = height;
+        this.width = width;
+        this.dir = dir;
+    }
+
+    public Dir getDir() {
+        return dir;
+    }
+
+    public void setDir(Dir dir) {
+        this.dir = dir;
+    }
+
+    public void paint(Graphics g) {
+        g.fillRect(x,y,width,height);
+        switch(dir){
+            case LEFT : x-=SPEED;
+            break;
+            case RIGHT: x+=SPEED;
+            break;
+            case UP: y-=SPEED;
+            break;
+            case DOWN: y+=SPEED;
+            break;
+            default:break;
+        }
+    }
+}
