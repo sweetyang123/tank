f12d0ecd140d654d4565cc4f55344141f890f313
sweetyang123 <84315764+sweetyang123@users.noreply.github.com>
2021-05-18 13:59:04 +0800

    Create Bullet.java
    子弹
    
    Co-Authored-By: yangtian <2020176+yangtian@users.noreply.github.com>

---
+++ src/main/java/frame/Bullet.java
@@ -0,0 +1,33 @@
+package frame;
+
+import java.awt.*;
+
+/**
+ * 子弹
+ */
+public class Bullet {
+    private int x,y;
+    private int width,height;
+    private Dir dir =Dir.DOWN;
+    private static  final int SPEED=50;
+    private boolean moving=false;
+
+
+    public Bullet(int x, int y, int width, int height, Dir dir) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.dir = dir;
+    }
+    public void paint(Graphics g) {
+        Color c= g.getColor();
+        g.setColor(Color.GREEN);
+        g.fillOval(x,y,width,height);
+        g.setColor(c);
+        move();
+    }
+    private void move(){
+      y+=SPEED;
+    }
+}
