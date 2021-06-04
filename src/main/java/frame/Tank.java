43f9d3435b80d63ffafdc52f1be3f55d1693628b
sweetyang123 <84315764+sweetyang123@users.noreply.github.com>
2021-06-04 17:34:27 +0800

    解决矩形创建问题说明

--- src/main/java/frame/Tank.java
+++ src/main/java/frame/Tank.java
@@ -17,7 +17,7 @@ public class Tank {
     private FireStrategy fs;
 
 
-
+    //解决每次碰撞时创建：一开始就创建对象，再跟随tank对象，移动和初始化时赋值
     private  Rectangle tankRect=new Rectangle();
 //    private  Rectangle tankRect=new Rectangle(this.x,this.y,width,height);
 
