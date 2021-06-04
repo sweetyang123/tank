b83cde19c3d25098d5c8432d4825eb0646545800
sweetyang123 <84315764+sweetyang123@users.noreply.github.com>
2021-06-04 15:34:50 +0800

    GameModel实现每面facade模式，且充当MVC的M，外来的都与他互动，TestFrame只需要画图

--- src/main/java/frame/TestFrame.java
+++ src/main/java/frame/TestFrame.java
@@ -7,18 +7,11 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.util.ArrayList;
-import java.util.List;
 
 public class TestFrame  extends Frame {
 
-    Tank myTank = new Tank(100,50,Group.GOOD,Dir.DOWN,this);
-    List<Bullet> bullets= new ArrayList<Bullet>();
-    List<Tank> tanks= new ArrayList<Tank>();
-    List<Explode> explodes=new ArrayList<Explode>();
-   // Bullet bullet = new Bullet(150,150,20,20,Dir.DOWN);
-    static final int GAME_WIDTH = PropertyMgr.getInt("GAME_WIDTH"),
-           GAME_HEIGHT = PropertyMgr.getInt("GAME_HEIGHT");
+    GameModel gm = new GameModel();
+
     public TestFrame(){
         // Frame f =new Frame();
         setVisible(true);
@@ -32,57 +25,23 @@ public class TestFrame  extends Frame {
         });
         addKeyListener(new myKeyListener());
     }
+
     @Override
-    public void paint(Graphics g){
-        System.out.println("paint");
-        Color c = g.getColor();
-        g.setColor(Color.MAGENTA);
-        g.drawString("子弹数量"+bullets.size(),30,50);
-        g.drawString("坦克数量"+tanks.size(),30,60);
-        g.drawString("爆炸数量"+explodes.size(),30,70);
-        g.setColor(c);
-        myTank.paint(g);
-        for (int i = 0; i <bullets.size() ; i++) {
-            bullets.get(i).paint(g);
-        }
-        for (int i = 0; i <tanks.size() ; i++) {
-            tanks.get(i).paint(g);
-        }
-        //给每个子弹与坦克进行碰撞检测
-        for (int i = 0; i <tanks.size() ; i++) {
-            for (int j = 0; j <bullets.size() ; j++) {
-                tanks.get(i).collWith(bullets.get(j));
-            }
-        }
-        for (int i = 0; i <explodes.size() ; i++) {
-            explodes.get(i).paint(g);
-        }
-//        explode.paint(g);
-//        Audio audio=new Audio("audio/explode.wav");
-//        audio.play();
-
-//        for (Bullet b:bullets) {
-//            b.paint(g);
-//            }
-//            //迭代器迭代移除画面外的子弹
-//        for (Iterator<Bullet> it = bullets.iterator(); it.hasNext(); ) {
-//            Bullet iterator = it.next();
-//            if (!iterator.living)it.remove();
-//            //else iterator.paint(g);
-//        }
-
+    public void paint(Graphics g) {
+        gm.paint(g);
     }
+
     Image offScreenImage = null;
 //解决闪烁
     @Override
     public void update(Graphics g) {
         if (offScreenImage == null) {
-            offScreenImage = this.createImage(GAME_WIDTH, GAME_HEIGHT);
+            offScreenImage = this.createImage(gm.GAME_WIDTH, gm.GAME_HEIGHT);
         }
         Graphics gOffScreen = offScreenImage.getGraphics();
         Color c = gOffScreen.getColor();
         gOffScreen.setColor(Color.BLACK);
-        gOffScreen.fillRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+        gOffScreen.fillRect(0, 0,gm.GAME_WIDTH, gm.GAME_HEIGHT);
         gOffScreen.setColor(c);
         paint(gOffScreen);
         g.drawImage(offScreenImage, 0, 0, null);
@@ -104,11 +63,11 @@ public class TestFrame  extends Frame {
                     break;
                 case KeyEvent.VK_DOWN:BD=true;
                     break;
-                case KeyEvent.VK_CONTROL:myTank.fire();
+                case KeyEvent.VK_CONTROL:gm.myTank.fire();
                     break;
                 default: break;
             }
-            setMainTankDir();
+            gm.setMainTankDir(BL,BR,BU,BD);
         }
 
         @Override
@@ -125,18 +84,8 @@ public class TestFrame  extends Frame {
                     break;
                 default: break;
             }
-            setMainTankDir();
+            gm.setMainTankDir(BL,BR,BU,BD);
         }
-        private void setMainTankDir(){
-            if (!BL&&!BR&&!BU&&!BD)myTank.setMoving(false);
-            else {
-                myTank.setMoving(true);
-                if (BL)myTank.setDir(Dir.LEFT);
-                if (BR)myTank.setDir(Dir.RIGHT);
-                if (BU)myTank.setDir(Dir.UP);
-                if (BD)myTank.setDir(Dir.DOWN);
-            }
 
-        }
     }
 }
